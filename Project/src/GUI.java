8339a2fabe8d5f40ede7743755d4ac38461a601e
paamt-iscte <paamt@iscte-iul.pt>
2021-10-14 20:59:47 +0100

    msg

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -1,12 +1,20 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class GUI{
     public GUI(){
         JFrame frame = new JFrame();
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
-        panel.setLayout(new GridLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 10, 10));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Project");
+        frame.pack();
+        frame.setVisible(true);
+
     }
 
     public static void main(String[] args){
