f1949ee6fbf15f6200591de5d64f33a037558428
paamt-iscte <paamt@iscte-iul.pt>
2021-10-15 17:17:14 +0100

    z

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,37 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GUI{
-    JFrame frame;
-    JPanel panel;
-    JPasswordField pw;
+public class GUI extends JFrame{
+    private JPasswordField passwordField1;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton button1;
 
     public GUI(){
-        frame = new JFrame();
-        panel = new JPanel();
-        pw = new JPasswordField(15);
-        pw.setMaximumSize(new Dimension(3,2));
-        pw.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.BLACK));
-        pw.setEchoChar('*');
-
-
-        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 10, 10));
-        panel.setLayout(new GridLayout(0,1));
-        panel.setBackground(Color.GRAY);
-        panel.add(pw);
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Project");
-        frame.pack();
-        frame.setVisible(true);
-
-        frame.add(pw);
+        add(panel1);
+        validate();
     }
 
     public static void main(String[] args) {
-        new GUI();
-
+        GUI gui = new GUI();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(300, 300);
+        gui.setVisible(true);
+        gui.pack();
+        gui.setTitle("TrelloHub");
     }
 
 }
+
