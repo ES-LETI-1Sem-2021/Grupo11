4799d1c215f6648355b7af9a4be25b7c93450f04
paamt-iscte <paamt@iscte-iul.pt>
2021-10-22 15:47:34 +0100

    novo update da GUI inicial

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -10,38 +10,41 @@ public class GUI extends JFrame{
     private JLabel text;
     private JButton iniciarLogInButton;
     private JButton button;
+    private JButton button_github;
+    private JButton button_trello;
 
     public GUI(String title){
         super(title);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
         this.setResizable(false);
+        button_github.setVisible(false);
+        button_trello.setVisible(false);
+        iniciarLogInButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                iniciarLogInButton.setVisible(false);
+                button_github.setVisible(true);
+                button_trello.setVisible(true);
+                }
+        });
+        button_trello.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        button_github.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
     }
 
-    public static void main(String[] args) throws Exception{
-        final URI uri = new URI("https://github.com/miguelrato18/ES-LETI-1Sem-2021-Grupo11");
-
+    public static void main(String[] args){
         JFrame frame = new GUI("TrelloHub");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setVisible(true);
-        JButton button= new JButton();
-
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (Desktop.isDesktopSupported()) {
-                    Desktop desktop = Desktop.getDesktop();
-                    try {
-                        desktop.browse(uri);
-                    } catch (Exception ex) {
-                    }
-                } else {
-                }
-            }
-        });
     }
 
 }
