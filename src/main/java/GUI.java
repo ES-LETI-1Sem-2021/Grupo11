3654f28185e8ddb85635311dbbec06ab884d531f
miguelrato18 <miguelrato18@gmail.com>
2021-10-22 15:42:25 +0100

    msg

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -11,18 +11,31 @@ public class GUI extends JFrame{
     private JButton iniciarLogInButton;
     private JButton button;
 
-    public GUI(String title){
+    public GUI(String title) throws URISyntaxException {
         super(title);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
-        this.setResizable(false);
+        this.setResizable(true);
 
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                URI uri;
+                try {
+                    uri = new URI("https://github.com/miguelrato18/ES-LETI-1Sem-2021-Grupo11");
+                    openWebpage(uri);
+                } catch (URISyntaxException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
     }
-
+    
+    
+    
     public static void main(String[] args) throws Exception{
-        final URI uri = new URI("https://github.com/miguelrato18/ES-LETI-1Sem-2021-Grupo11");
 
         JFrame frame = new GUI("TrelloHub");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,19 +43,32 @@ public class GUI extends JFrame{
         frame.setVisible(true);
         JButton button= new JButton();
 
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (Desktop.isDesktopSupported()) {
-                    Desktop desktop = Desktop.getDesktop();
-                    try {
-                        desktop.browse(uri);
-                    } catch (Exception ex) {
-                    }
-                } else {
-                }
+    }
+//encontrado num forum
+    public static boolean openWebpage(URI uri) {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                desktop.browse(uri);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }
+        return false;
     }
 
+    public static boolean openWebpage(URL url) {
+        try {
+            return openWebpage(url.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
 
