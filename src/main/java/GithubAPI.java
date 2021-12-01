5cab7136d841e8ecb2ee7c2932cb2b6faeb96ebe
miguelrato18 <miguelrato18@gmail.com>
2021-12-01 16:32:16 +0000

    API GitHub ja a funcionar parcialmente: nomes dos participantes nmo projeto. fichewiro readme. etiquetas. commits.

--- src/main/java/GithubAPI.java
+++ src/main/java/GithubAPI.java
@@ -1,6 +1,47 @@
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class GithubAPI {
 
+    private static int NrActivityWArt;
+    private static int NrActivityWOArt;
+    private static String Readme;
+    private static List<String> TagList;
+    private static List<String[]> CommitInfo;
+    private static List<String[]> MemberInfo;
+    private GitHub github;
 
 
+    public GithubAPI(String password_token_text) throws IOException {
+        github = new GitHubBuilder().withOAuthToken(password_token_text).build();
+
+
+    }
+//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
+
+
+    public void getProjectDiscription() throws IOException {
+         GHRepository GitRepo = github.getRepository("miguelrato18/ES-LETI-1Sem-2021-Grupo11");
+
+
+        //nomes de que ta no project
+        System.out.println(GitRepo.getCollaboratorNames().toString());
+        //ficheiro readme
+        System.out.println(GitRepo.getFileContent("README.md").getContent());
+        //etiquetas
+        System.out.println(GitRepo.listTags());
+        //commits
+        for (int i = 0; i < GitRepo.listCommits().toList().size();i++){
+            System.out.println(GitRepo.listCommits().toList().get(i).getCommitShortInfo().getAuthor().getName());
+            System.out.println(GitRepo.listCommits().toList().get(i).getCommitShortInfo().getCommitDate());
+        }
+
+
+    }
 
 }
